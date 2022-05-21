bea7b20cff3954f811e18c5837c69b1d0ce98e6c
aviwol <aviwolicki@gmail.com>
2022-05-22 00:25:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
+const http = require("http");
 const fs = require('fs');
-const fastify = require('fastify')({ logger: false })
+const client = require('redis').createClient()
+const cards = JSON.parse(fs.readFileSync('./cards.json'));
+const fastJson = require('fast-json-stringify');
 const port = +process.argv[2] || 3000
 
-const client = require('redis').createClient()
+const host = '127.0.0.1';
 
-client.on('ready', () => {
-    fastify.listen(port)
+const stringify = fastJson({
+  title: 'Example Schema',
+  type: 'object',
+  properties: {
+    id: {
+      type: 'string'
+    },
+    name: {
+      type: 'string'
+    }
+  }
 })
 
-const cards = JSON.parse(fs.readFileSync('./cards.json'));
+const requestListener = async function (req, res) {
+    const requesturl = req.url;
+    if (requesturl.indexOf("/c") > -1){
+        let uuid = requesturl.substr(13);
+        const index = await client.INCR(uuid);
+        let missingCard = cards[index - 1]; 
 
-fastify.get('/card_add', async (req, res) => {
-    const index = await client.INCR(req.query.id)
-    let missingCard = cards[index - 1];
-    return missingCard === undefined ? {"id": "ALL CARDS"} : missingCard;
-})
+        if(missingCard === undefined){
+            response = '{"id": "ALL CARDS"}'
+        } else {
+            response = stringify(missingCard);            
+        }
+    
+        res.writeHead(200);
+        res.end(response);
 
-fastify.get('/ready', async (request, reply) => {
-  return {"ready": true}
-})
+    } else {
+        res.writeHead(200);
+        res.end('{"ready": true}');        
+    }
+};
 
+const server = http.createServer(requestListener);
+server.listen(port, host);
 client.connect();
