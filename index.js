7e254e87d020201751e3f32246db5f116020f7a0
aviwol <aviwolicki@gmail.com>
2022-05-22 11:46:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const turbo = require("turbo-http");
 const fs = require('fs');
-const client = require('redis').createClient()
 const cards = JSON.parse(fs.readFileSync('./cards.json'));
 const fastJson = require('fast-json-stringify');
 const port = +process.argv[2] || 3000
@@ -20,29 +19,39 @@ const stringify = fastJson({
   }
 })
 
+const indexes = {};
+if(port === 4002){
+    max = 100
+    start = 50
+} else {
+    max = 50
+    start = 0
+}
+
 
 const server = turbo.createServer(async function (req, res) {
     requesturl = req.url;
     if (requesturl.indexOf("/re") > -1){
         response = '{"ready": true}'
         res.setHeader('Content-Length', 15)
-        res.write(response); 
+        res.end(response); 
 
     } else {
-        let uuid = requesturl.substr(13);
-        let index = await client.INCR(uuid);
+        if(indexes[requesturl] === undefined){
+            indexes[requesturl] = start
+        }
+        const index = ++indexes[requesturl];
         let missingCard = cards[index-1]; 
 
-        if(missingCard === undefined){
-            response = '{"id": "ALL CARDS"}'
-            res.setHeader('Content-Length', 19)
-        } else {
+        if(index <= max){
             response = stringify(missingCard);  
             res.setHeader('Content-Length', 91)
+        } else {
+            response = '{"id": "ALL CARDS"}'
+            res.setHeader('Content-Length', 19)
         }    
-        res.write(response);        
+        res.end(response);        
     }
 })
 
 server.listen(port)
-client.connect();
